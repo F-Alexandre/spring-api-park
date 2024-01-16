71209ed592268fca666c37c263f92d42f2bc8629
special_code <fellipinho2012@gmail.com>
2024-01-16 17:22:41 -0300

    implementing tests

---
+++ src/test/resources/sql/users/user-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS(id , username, password, role)values(101,"teste@example.com" , "123456");
+insert into USUARIOS(id , username, password, role)values(102,"teste2@example.com" , "1234567");
+insert into USUARIOS(id , username, password, role)values(103,"teste3@example.com" , "1234568");
